f9acd6bfff28c96f3345546b260d70c3b7125cb0
Mustafa Ezer <mstfezer@gmail.com>
2020-07-16 10:50:03 +0300

    Add fetching data from models to UI

--- webapp/views.py
+++ webapp/views.py
@@ -1,10 +1,15 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
+from webapp.models import AccessRecord, Topic, Webpage
 
 
 def index(request):
+    webpagesList = AccessRecord.objects.order_by('date')
+    
     dictionary = {
+        'accessRecords': webpagesList,
         'title': 'Hello, Im a title coming from webapp/views.py!'
     }
+
     return render(request,  'webapp/home.html', context=dictionary)
